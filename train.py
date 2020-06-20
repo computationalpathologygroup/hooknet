2302fae7a3866979467c02147b08d0da002e1434
martvanrijthoven <mart.vanrijthoven@gmail.com>
2020-06-20 10:30:09 +0200

    add comments

--- train.py
+++ train.py
@@ -1,28 +1,52 @@
-from argconfigparser import parse
+import argconfigparser
 from source.model import HookNet
 from source.generator.batchgenerator import RandomBatchGenerator
 from source.trainer import Trainer
 
 
 def train():
-    config = parse('./parameters.yml')
+    """
+    train function
 
+    This train function is made for illustration and testing purposes only, as it uses a RandomBatchGenerator (i.e., random generated data)
+    
+    """
+
+    # parse config and command line arguments
+    config = argconfigparser.parse('./parameters.yml')
+    print(f'CONFIG: \n{config}')
+
+    # initialize model
     hooknet = HookNet(input_shape=config['input_shape'],
                       n_classes=config['n_classes'],
-                      hook_indexes=config['hook_indexes'])
+                      hook_indexes=config['hook_indexes'],
+                      depth=config['depth'],
+                      n_convs=config['n_convs'],
+                      filter_size=config['filter_size'],
+                      n_filters=config['n_filters'],
+                      padding=config['padding'],
+                      batch_norm=config['batch_norm'],
+                      activation=config['activation'],
+                      learning_rate=config['learning_rate'],
+                      opt_name=config['opt_name'],
+                      l2_lambda=config['l2_lambda'],
+                      loss_weights=config['loss_weights'],
+                      merge_type=config['merge_type'])
 
+    # initialize batchgenerator
     batchgenerator = RandomBatchGenerator(batch_size=config['batch_size'],
                                           input_shape=hooknet.input_shape,
                                           output_shape=hooknet.output_shape,
                                           n_classes=config['n_classes'])
 
-    trainer = Trainer(epochs=config['epochs'],
-                      steps=config['steps'],
-                      batch_size=config['batch_size'],
-                      model=hooknet,
+    # initialize trainer
+    trainer = Trainer(model=hooknet,
                       batch_generator=batchgenerator,
-                      seed=config['seed'])
+                      epochs=config['epochs'],
+                      steps=config['steps'],
+                      batch_size=config['batch_size'])
 
+    # train
     trainer.train()
 
 
